dd33418b72777d5ff2e5c5bc5b532ac9eb615a6f
chen <569844962@qq.com>
2016-05-29 22:50:47 +0800

    2016/5/29 12.1

--- CriminalIntent/src/com/chen/example/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/com/chen/example/criminalintent/CrimeFragment.java
@@ -6,10 +6,12 @@ import com.chen.example.criminalintent.modle.Crime;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -18,6 +20,7 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 public class CrimeFragment extends Fragment {
 	public static final String EXTRA_CRIME_ID="com.chen.example.criminalintent.crime_id";
+	private final static String DIALOG_DATE="date";
 	private Crime mCrime;
 	private EditText mTitleFiled;
 	private Button mDateButton;
@@ -67,7 +70,15 @@ public class CrimeFragment extends Fragment {
 		});
 		mDateButton=(Button) v.findViewById(R.id.crime_date);
 		mDateButton.setText(mCrime.getDate().toString());
-		mDateButton.setEnabled(false);
+		mDateButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FragmentManager fm=getActivity().getSupportFragmentManager();
+				DatePickerFragment dialog=new DatePickerFragment();
+				dialog.show(fm, DIALOG_DATE);
+			}
+		});
 		mSolvedCheckBox=(CheckBox) v.findViewById(R.id.crime_solved);
 		mSolvedCheckBox.setChecked(mCrime.isSolved());
 		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
